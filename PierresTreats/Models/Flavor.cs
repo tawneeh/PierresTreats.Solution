feaf97243943744e100bc0e0895530486f80edb8
Tawnee Harris <tawneeh@icloud.com>
2021-01-15 08:50:41 -0800

    add Flavor model

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class Flavor
+  {
+    public Flavor()
+    {
+      this.Treats = new HashSet<FlavorTreat>();
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<FlavorTreat> Treats { get; set; }
+
+  }
+}
