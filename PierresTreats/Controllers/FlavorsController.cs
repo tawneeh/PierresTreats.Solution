272b669a9f2ff7bd8cfb4c6c71e1f7d1feba4a30
Tawnee Harris <tawneeh@icloud.com>
2021-01-15 10:47:09 -0800

    add DeleteTreat HttpPost route

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -72,5 +72,14 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpPost]
+    public ActionResult DeleteTreat(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
   }
 }
