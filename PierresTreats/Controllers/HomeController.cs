b9d55c903d6307738b3e72197fdc27a7ffa95ee1
Tawnee Harris <tawneeh@icloud.com>
2021-01-15 09:18:30 -0800

    add HomeController

---
+++ PierresTreats/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Linq;
+
+namespace PierresTreats.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public HomeController(PierresTreatsContext _db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      ViewBag.Treat = _db.Treats.ToList();
+      ViewBag.Flavor = _db.Flavors.ToList();
+      return View();
+    }
+
+  }
+}
