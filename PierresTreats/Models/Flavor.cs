bae0799d7830aac4a51b09e0898a3c245ac68b5e
Tawnee Harris <tawneeh@icloud.com>
2021-01-15 16:04:24 -0800

    add ApplicationUser to Flavor

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -8,9 +8,9 @@ namespace PierresTreats.Models
     {
       this.Treats = new HashSet<FlavorTreat>();
     }
-
     public int FlavorId { get; set; }
     public string Name { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<FlavorTreat> Treats { get; set; }
 
   }
