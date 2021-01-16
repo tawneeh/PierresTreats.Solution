2bdfa6904c373a3a5e92095248d2dce2f2438da1
Tawnee Harris <tawneeh@icloud.com>
2021-01-15 16:51:59 -0800

    add Authorize to Details. track down bug

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -50,7 +50,10 @@ namespace PierresTreats.Controllers
       var thisFlavor = _db.Flavors
           .Include(flavor => flavor.Treats)
           .ThenInclude(join => join.Treat)
+          .Include(flavor => flavor.User)
           .FirstOrDefault(flavor => flavor.FlavorId == id);
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ViewBag.IsCurrentUser = userId != null ? userId == thisFlavor.User.Id : false;
       return View(thisFlavor);
     }
 
